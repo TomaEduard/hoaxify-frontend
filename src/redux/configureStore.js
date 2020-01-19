a705c9c93a071630fcc0eb90d74334828879dcb2
Eduard Toma <eduard.daniel.toma@gmail.com>
2020-01-19 10:06:34 +0200

    added react thunk to trigger the localStorage and set item in it with reducer values

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,9 +5,36 @@ import logger from 'redux-logger'; //optional for consol.log
 import thunk from 'redux-thunk';
 
 const configureStore = (addLogger = true) => {
+
+    let localStorageData = localStorage.getItem('hoax-auth');
+
+    let persistedState = {
+        id: 0,
+        username: '',
+        displayName: '',
+        image: '',
+        password: '',
+        isLoggedIn: false
+    };
+    if (localStorageData) {
+        try {
+            persistedState = JSON.parse(localStorageData);
+        } catch (error) {
+
+        }
+    }
+
     // if addLogger =  true add additional logger
     const middleware = addLogger ? applyMiddleware(thunk, logger) : applyMiddleware(thunk)
-    return createStore(authReducer, middleware);
+    const store = createStore(authReducer, persistedState, middleware);
+
+    // this will be call whenever anything change in the redux store(authReducer)
+    // save the current store in local storage
+    store.subscribe(() => {
+        localStorage.setItem('hoax-auth', JSON.stringify(store.getState()));
+    })
+
+    return store;
 };
 
 export default configureStore;
