a2f507b0abdcc2f776ebc5050ca971ea0c7b0e94
Eduard Toma <eduard.daniel.toma@gmail.com>
2020-01-20 09:47:13 +0200

    added profile image and set default one if not have image

---
+++ src/components/UserListItem.spec.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import UserListItem from './UserListItem';
+
+const user = {
+    username: 'user1',
+    displayName: 'display1',
+    image: 'profile1.png'
+ };
+
+describe('UserListItem', () => {
+
+    it('has image', () => {
+        const { container } = render(<UserListItem user={user}/>);
+        const image = container.querySelector('img');
+        expect(image).toBeInTheDocument();
+    });
+
+    it('display default image when user does not have one', () => {
+        const userWithoutImage = {
+            ...user,
+            image: undefined
+        }
+
+        const { container } = render(<UserListItem user={userWithoutImage}/>);
+        const image = container.querySelector('img');
+        expect(image.src).toContain('/profile.png');
+    });
+
+    it('display users image when user have one', () => {
+        const { container } = render(<UserListItem user={user}/>);
+        const image = container.querySelector('img');
+        expect(image.src).toContain('/images/profile/' + user.image);
+    });
+});
